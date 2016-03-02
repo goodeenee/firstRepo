ae4d76f9fe69de70f65b6f6fab6f9488812f61d0
goodeenee <goodeenee@gmail.com>
2016-03-03 00:51:44 +0200

    add BlaBla class add new sout

---
+++ src/Request/VkMethod.java
@@ -0,0 +1,32 @@
+package Request;
+
+public class VkMethod {
+
+        //Users
+        public static final String USERS_GET="users.get";
+        public static final String USERS_SEARCH = "users.search";
+        public static final String USERS_IS_APP_USER = "users.isAppUser";
+        public static final String USERS_GET_SUBSCRIPTIONS = "users.getSubscriptions";
+        public static final String USERS_GET_FOLLOWERS = "users.getFollowers";
+        public static final String USERS_REPORT = "users.report";
+        public static final String USERS_GET_NEARBY = "users.getNearby";
+
+        //Authorization
+        public static final String AUTH_CHECK_PHONE = "auth.checkPhone";
+        public static final String AUTH_SIGNUP = "auth.signup";
+        public static final String AUTH_CONFIRM = "auth.confirm";
+        public static final String AUTH_RESTORE = "auth.restore";
+
+        //Wall
+        public static final String WALL_GET = "wall.get";
+        public static final String WALL_SEARCH = "wall.search";
+        public static final String WALL_GET_BY_ID = "wall.getById";
+        public static final String WALL_POST = "wall.post";
+        public static final String WALL_REPOST = "wall.repost";
+        public static final String WALL_GET_REPOSTS = "wall.getReposts";
+        public static final String WALL_EDIT = "wall.edit";
+        public static final String WALL_DELETE = "wall.delete";
+        public static final String WALL_RESTORE = "wall.restore";
+        public static final String WALL_PIN = "wall.pin";
+        public static final String WALL_UNPIN = "wall.unpin";
+}
